3f9932db4c90bdaae2a9ef6b6870ac3d756d6d5f
jiyun park <zzzwe203@naver.com>
2019-11-22 15:48:40 +0900

    sss
    sss

---
+++ config/database.js
@@ -0,0 +1,5 @@
+module.exports = {
+  database: 'mongodb+srv://hackdun:wldbs1212@cluster0-sasbp.mongodb.net/test?retryWrites=true&w=majority',
+  // database: 'mongodb://sultan:sultan123456@ds151066.mlab.com:51066/meanauth2019',
+  secret: 'yoursupersecret-dfljksdlfkj'
+}
